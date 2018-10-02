3e38c7e4d5afe4ac791b33f35d2829ead019df71
Isaac Rhett <isaac.rhett@lookout.com>
2018-10-02 11:12:31 -0400

    [hack.sh] [blocking_hook.sh]

--- bash/blocking_hook.sh
+++ bash/blocking_hook.sh
@@ -1,5 +1,15 @@
-FILES_PATTERN=''
+FILES_PATTERN=''  # empty string matches all files
 FORBIDDEN='DONOTCOMMIT'
+GIT_ROOT=""
+while [ -z "${GIT_ROOT}" ] ; do 
+  GIT_ROOT="$(git root 2> /dev/null)"
+  if [ "$?" ] ; then
+    git config --global alias.root 'rev-parse --show-toplevel'
+  fi
+done
+
 git diff --cached --name-only | \
+  sed "s!.*!$GIT_ROOT/&!g" | \
   grep -E "$FILES_PATTERN" | \
-  xargs grep --with-filename --ignore-case --line-number "$FORBIDDEN"s && echo "COMMIT REJECTED Found '$FORBIDDEN' references. Please remove them before commiting" && exit 1
+  xargs grep --with-filename --ignore-case --line-number --no-messages "$FORBIDDEN" | \
+  grep --ignore-case --color=always "$FORBIDDEN" && echo -e "COMMIT REJECTED:\n\tFound '$FORBIDDEN' references.\n\tPlease remove them before commiting" && exit 1
--- bash/hack.sh
+++ bash/hack.sh
@@ -1,1 +1,1 @@
-alias hack=":(){sleep .5;  echo -n . }; echo -n \"Hacking\" ;:;:;:; sleep .5 ; echo -n \"Done\!\" ; sleep 1 ; echo \"\" ; echo > /dev/null"
+alias hack="hackingdelay () { sleep .5 ; echo -n . }; echo -n \"Hacking\" ;hackingdelay;hackingdelay;hackingdelay; sleep .5 ; echo -n \"Done\!\" ; sleep 1 ; echo \"\" ; echo > /dev/null"
