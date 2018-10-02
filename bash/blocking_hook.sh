ca5f949af8416ea9af72b0991ebb888290cb6aa5
Isaac Rhett <isaac.rhett@lookout.com>
2018-10-02 11:16:56 -0400

    [blocking_hook.sh]

--- bash/blocking_hook.sh
+++ bash/blocking_hook.sh
@@ -1,5 +1,6 @@
 FILES_PATTERN=''  # empty string matches all files
 FORBIDDEN='DONOTCOMMIT'  # offending string
+MESSAGE="COMMIT REJECTED:\n\tFound '$FORBIDDEN' references.\n\tPlease remove them before commiting"
 
 # so files can be referenced by absolute path
 GIT_ROOT=""
@@ -14,4 +15,5 @@ git diff --cached --name-only | \
   sed "s!.*!$GIT_ROOT/&!g" | \
   grep -E "$FILES_PATTERN" | \
   xargs grep --with-filename --ignore-case --line-number --no-messages "$FORBIDDEN" | \
-  grep --ignore-case --color=always "$FORBIDDEN" && echo -e "COMMIT REJECTED:\n\tFound '$FORBIDDEN' references.\n\tPlease remove them before commiting" && exit 1
+  grep --ignore-case --color=always "$FORBIDDEN" && echo -e "$MESSAGE" && exit 1
+exit 0
