f4cb7aa9e07d35e4326d344c7d6bce64aaa238ad
Isaac Rhett <isaac.rhett@lookout.com>
2019-06-19 14:33:15 -0400

    +[array.sh]

---
+++ bash/array.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -a INDEXED_ARR_VAR
+
+INDEXED_ARR=(foo bar)
+INDEXED_ARR[2]=baz
+
+echo "${INDEXED_ARR[*]}"
+echo "${#INDEXED_ARR[@]}"
+
+for i in "${INDEXED_ARR[@]}" ; do 
+  echo "$i" 
+  echo "$i"
+done
+
+for i in "${!INDEXED_ARR[@]}" ; do
+  echo "$i"
+done
+
